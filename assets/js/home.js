1d4b94b371af3188f2549ef38c6075f35f917d0f
Satria Fikri Hidayatullah <79389790+beffstick@users.noreply.github.com>
2021-03-05 15:52:13 +0800

    Add files via upload

---
+++ assets/js/home.js
@@ -0,0 +1,75 @@
+function myFunction() {
+  var konten1 = document.getElementById("konten1");
+  var readmore = document.getElementById("readmore");
+  var pencet = document.getElementById("pencet");
+
+  if (konten1.style.display === "none") {
+    konten1.style.display = "inline";
+    pencet.innerHTML = "Read more";
+    readmore.style.display = "none";
+  } else {
+    konten1.style.display = "none";
+    pencet.innerHTML = "Read less";
+    readmore.style.display = "inline";
+  }
+}
+function myFunction1() {
+  var konten2 = document.getElementById("konten2");
+  var readmore2 = document.getElementById("readmore2");
+  var pencet2 = document.getElementById("pencet2");
+
+  if (konten2.style.display === "none") {
+    konten2.style.display = "inline";
+    pencet2.innerHTML = "Read more";
+    readmore2.style.display = "none";
+  } else {
+    konten2.style.display = "none";
+    pencet2.innerHTML = "Read less";
+    readmore2.style.display = "inline";
+  }
+}
+function myFunction2() {
+  var konten3 = document.getElementById("konten3");
+  var readmore3 = document.getElementById("readmore3");
+  var pencet3 = document.getElementById("pencet3");
+
+  if (konten3.style.display === "none") {
+    konten3.style.display = "inline";
+    pencet3.innerHTML = "Read more";
+    readmore3.style.display = "none";
+  } else {
+    konten3.style.display = "none";
+    pencet3.innerHTML = "Read less";
+    readmore3.style.display = "inline";
+  }
+}
+function myFunction3() {
+  var konten4 = document.getElementById("konten4");
+  var readmore4 = document.getElementById("readmore4");
+  var pencet4 = document.getElementById("pencet4");
+
+  if (konten4.style.display === "none") {
+    konten4.style.display = "inline";
+    pencet4.innerHTML = "Read more";
+    readmore4.style.display = "none";
+  } else {
+    konten4.style.display = "none";
+    pencet4.innerHTML = "Read less";
+    readmore4.style.display = "inline";
+  }
+}
+function myFunction4() {
+  var konten5 = document.getElementById("konten5");
+  var readmore5 = document.getElementById("readmore5");
+  var pencet5 = document.getElementById("pencet5");
+
+  if (konten5.style.display === "none") {
+    konten5.style.display = "inline";
+    pencet5.innerHTML = "Read more";
+    readmore5.style.display = "none";
+  } else {
+    konten5.style.display = "none";
+    pencet5.innerHTML = "Read less";
+    readmore5.style.display = "inline";
+  }
+}
